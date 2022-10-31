a9ca93b06c90a2b0d2d89052ed0d7108f2e0753a
Pedro Guerreiro <pmoreiraguerreiro@gmail.com>
2022-10-31 12:14:04 +0000

    Add speciesSlice with fetchSpecies function

--- src/app/store.js
+++ src/app/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import vetsReducer from './vets';
 import ownersReducer from './owners';
+import speciesReducer from './species';
 
 const store = configureStore({
   reducer: {
     vets: vetsReducer,
     owners: ownersReducer,
+    species: speciesReducer,
   },
 });
 
